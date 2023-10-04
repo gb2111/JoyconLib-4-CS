5da22dae092b1cc26a56ce472e86f213bea665f7
gb2111 <knurgb@gazeta.pl>
2023-10-04 09:04:40 +0200

    Fix for showing only the 2nd connect Joy-Con

--- src/MainForm.cs
+++ src/MainForm.cs
@@ -14,107 +14,106 @@ using System.Windows.Forms;
 
 namespace JoyConTest
 {
-	public partial class MainForm : Form
-	{
-		public MainForm()
-		{
-			InitializeComponent();
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
 
-			drawOrigin = new Point(picture1.Width / 2, picture1.Height / 2);
-			cube1 = new Math3D.Math3D.Cube(200, 100, 20);
-			cube2 = new Math3D.Math3D.Cube(200, 100, 20);
+            drawOrigin = new Point(picture1.Width / 2, picture1.Height / 2);
+            cube1 = new Math3D.Math3D.Cube(200, 100, 20);
+            cube2 = new Math3D.Math3D.Cube(200, 100, 20);
 
-		}
+        }
 
-		JoyconManager joyconManager = new JoyconManager();
+        JoyconManager joyconManager = new JoyconManager();
 
-		Point drawOrigin;
-		Math3D.Math3D.Cube cube1;
-		Math3D.Math3D.Cube cube2;
-
-
-		private void buttonScan_Click(object sender, EventArgs e)
-		{
-			joyconManager.Scan();
-
-			UpdateDebugType();
-			UpdateInfo();
-
-		}
-
-		private void UpdateDebugType()
-		{
-			foreach (var j in joyconManager.j)
-				j.debug_type = Joycon.DebugType.NONE;
-		}
+        Point drawOrigin;
+        Math3D.Math3D.Cube cube1;
+        Math3D.Math3D.Cube cube2;
 
 
+        private void buttonScan_Click(object sender, EventArgs e)
+        {
+            joyconManager.Scan();
 
-		private void buttonStart_Click(object sender, EventArgs e)
-		{
-			joyconManager.Start();
-			timerUpdate.Enabled = true;
-		}
+            UpdateDebugType();
+            UpdateInfo();
 
-		private void timerUpdate_Tick(object sender, EventArgs e)
-		{
-			joyconManager.Update();
+        }
 
-			UpdateInfo();
-		}
-
-		private void UpdateInfo()
-		{
-			if (joyconManager.j.Count > 0)
-			{
-				var j = joyconManager.j[0];
-
-				label1.Text = j.ToString();
-
-				cube1.InitializeCube();
-				cube1.RotateX = (float)(j.GetVector().eulerAngles.Y * 180.0f / Math.PI);
-				cube1.RotateY = (float)(j.GetVector().eulerAngles.Z * 180.0f / Math.PI);
-				cube1.RotateZ = (float)(j.GetVector().eulerAngles.X * 180.0f / Math.PI);
-
-				picture1.Image = cube1.DrawCube(drawOrigin);
-			}
-			else
-			{
-				if (label1.Text != "")
-					label1.Text = "not found";
-			}
-
-			if (joyconManager.j.Count > 1)
-			{
-				var j = joyconManager.j[1];
-
-				label2.Text = j.ToString();
-				picture2.Image = cube2.DrawCube(drawOrigin);
-
-
-				cube2.InitializeCube();
-				cube2.RotateX = (float)(j.GetVector().eulerAngles.Y * 180.0f / Math.PI);
-				cube2.RotateY = (float)(j.GetVector().eulerAngles.Z * 180.0f / Math.PI);
-				cube2.RotateZ = (float)(j.GetVector().eulerAngles.X * 180.0f / Math.PI);
-
-				picture1.Image = cube2.DrawCube(drawOrigin);
-
-			}
-			else
-			{
-				if (label2.Text != "")
-					label2.Text = "not found";
-			}
+        private void UpdateDebugType()
+        {
+            foreach (var j in joyconManager.j)
+                j.debug_type = Joycon.DebugType.NONE;
+        }
 
 
 
-		}
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            joyconManager.Start();
+            timerUpdate.Enabled = true;
+        }
 
-		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
-		{
-			joyconManager.OnApplicationQuit();
-		}
+        private void timerUpdate_Tick(object sender, EventArgs e)
+        {
+            joyconManager.Update();
+
+            UpdateInfo();
+        }
+
+        private void UpdateInfo()
+        {
+            if (joyconManager.j.Count > 0)
+            {
+                var j = joyconManager.j[0];
+
+                label1.Text = j.ToString();
+
+                cube1.InitializeCube();
+                cube1.RotateX = (float)(j.GetVector().eulerAngles.Y * 180.0f / Math.PI);
+                cube1.RotateY = (float)(j.GetVector().eulerAngles.Z * 180.0f / Math.PI);
+                cube1.RotateZ = (float)(j.GetVector().eulerAngles.X * 180.0f / Math.PI);
+
+                picture1.Image = cube1.DrawCube(drawOrigin);
+            }
+            else
+            {
+                if (label1.Text != "")
+                    label1.Text = "not found";
+            }
+
+            if (joyconManager.j.Count > 1)
+            {
+                var j = joyconManager.j[1];
+
+                label2.Text = j.ToString();
 
 
-	}
+                cube2.InitializeCube();
+                cube2.RotateX = (float)(j.GetVector().eulerAngles.Y * 180.0f / Math.PI);
+                cube2.RotateY = (float)(j.GetVector().eulerAngles.Z * 180.0f / Math.PI);
+                cube2.RotateZ = (float)(j.GetVector().eulerAngles.X * 180.0f / Math.PI);
+
+                picture2.Image = cube2.DrawCube(drawOrigin);
+
+            }
+            else
+            {
+                if (label2.Text != "")
+                    label2.Text = "not found";
+            }
+
+
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            joyconManager.OnApplicationQuit();
+        }
+
+
+    }
 }
